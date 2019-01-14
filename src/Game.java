7051d5a0b8c1d3f0f30df7635c167824728aa245
Dillon Higa <dillonhiga@hotmail.com>
2019-01-14 12:42:15 -0800

    Basic Setup Complete

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable{
     private static final long serialVersionUID = -6112428091888191314L;
@@ -13,11 +14,60 @@ public class Game extends Canvas implements Runnable{
 
     @Override
     public void run() {
+        //game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (running) {
+                render();
+            }
+            frames++;
+
+            if ((System.currentTimeMillis() - timer) > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void render(){
+        BufferStrategy bufferStrategy = this.getBufferStrategy();
+
+        if(bufferStrategy == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+        graphics.setColor(Color.red);
+        graphics.fillRect(0,0,WIDTH,HEIGHT);
+
+
+        graphics.dispose();
+        bufferStrategy.show();
+    }
+
+    private void tick(){
 
     }
 
+
+
     public static void main(String[] args) {
-    new Game();
+        new Game();
     }
 
 
